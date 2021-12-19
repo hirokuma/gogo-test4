81548d9b4ef0864b579ca60ac3c4ca91f38e9966
hirokuma <hiro9ma@gmail.com>
2021-12-19 17:46:13 +0900

    for gogo-test3

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hirokuma/gogo-test4
+
+go 1.17
---
+++ gogo4.go
@@ -0,0 +1,13 @@
+package gogotest4
+
+var (
+	_val int
+)
+
+func SetValue(val int) {
+	_val = val
+}
+
+func GetValue() int {
+	return _val
+}
